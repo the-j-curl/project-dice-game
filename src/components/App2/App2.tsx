358261ca4ba469b5d2bede1d816d52480600d610
the-j-curl <jamie.p.cook@outlook.com>
2021-09-09 15:54:36 +0200

    Created react app

---
+++ src/components/App2/App2.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import styles from './styles.module.css';
+
+export const App2 = () => {
+  return (
+    <div className={styles.main}>
+      App 2
+    </div>
+  );
+};

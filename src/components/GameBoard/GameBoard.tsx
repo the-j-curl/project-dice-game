61388cc3bfc1549d3d11b4046ef0a3be4663cbc6
the-j-curl <jamie.p.cook@outlook.com>
2021-09-12 11:45:38 +0200

    Created types for reducers

--- src/components/GameBoard/GameBoard.tsx
+++ src/components/GameBoard/GameBoard.tsx
@@ -1,21 +1,25 @@
 import React, { useState } from 'react';
+import { useSelector } from 'react-redux';
 
 import { Button, Dice, PlayerCard } from 'components';
 import styles from './styles.module.css';
 
-export const GameBoard = () => {
-  const [randomNum, setRandomNum] = useState(0)
-  const diceButton = "Roll Dice"; // TODO: REMOVE THIS LINE
-  const playerName = 'Player Test'; // TODO: REMOVE THIS LINE
-  const score = 0; // TODO: REMOVE THIS LINE
+export const GameBoard: React.FC = () => {
+  const playerOne = useSelector((store: any) => store.game.playerOne);
+  const playerTwo = useSelector((store: any) => store.game.playerTwo);
+  const playerOneScore = useSelector((store: any) => store.game.totalScore.playerOne)
+  const playerTwoScore = useSelector((store: any) => store.game.totalScore.playerTwo)
 
-  const randomNumberGenerator = (min: number, max: number) => {
+  const [randomNum, setRandomNum] = useState(0);
+  const diceButton = "Roll Dice"; // TODO: REMOVE THIS LINE
+
+  const randomNumberGenerator = (min: number, max: number) => { // TODO: move to helper folder
     min = Math.ceil(min);
     max = Math.floor(max);
     return Math.floor(Math.random() * (max - min + 1) + min);
   };
 
-  const setDiceRoll = (min: number, max: number) => {
+  const setDiceRoll = (min: number, max: number) => { // TODO: move to helper folder
     let num = randomNumberGenerator(min, max);
     setRandomNum(num)
   };
@@ -23,7 +27,8 @@ export const GameBoard = () => {
   return (
     <main className={styles.boardWrapper}>
       <p>Game Board</p>
-      <PlayerCard playerName={playerName} totalScore={score} />
+      <PlayerCard playerName={playerOne} totalScore={playerOneScore} />
+      <PlayerCard playerName={playerTwo} totalScore={playerTwoScore} />
       <Dice />
       <Button buttonText={diceButton} onClickFunction={() => setDiceRoll(1, 6)} />
       <p>Random number: {randomNum}</p>
--- src/reducers/game.tsx
+++ src/reducers/game.tsx
@@ -1,12 +1,35 @@
 import { createSlice } from "@reduxjs/toolkit";
 
-const initialState = {
+interface TotalScore {
+  playerOne?: number
+  playerTwo?: number
+}
+interface Game {
+  playerOne: string
+  playerTwo: string
+  totalScore: TotalScore
+  isPlayerOneTurn: boolean
+}
+
+// interface UpdateNameAction {
+//   payload: string
+// }
+
+// interface ScoreAction {
+//   payload: number
+// }
+
+// interface TurnAction {
+//   payload: boolean
+// }
+
+interface Action {
+  payload: any
+}
+
+const initialState: Game = {
   playerOne: 'Player 1',
   playerTwo: 'Player 2',
-  currentScore: {
-    playerOne: 0,
-    playerTwo: 0,
-  },
   totalScore: {
     playerOne: 0,
     playerTwo: 0,
@@ -18,23 +41,23 @@ export const game = createSlice({
   name: 'game',
   initialState,
   reducers: {
-    updatePlayerName: (store, action) => {
-      const { playerOneName, playerTwoName } = action.payload;
-      store.playerOne = playerOneName;
-      store.playerTwo = playerTwoName;
-      localStorage.setItem('playerOne', playerOneName);
-      localStorage.setItem('playerTwo', playerTwoName);
+    updatePlayerName: (store: Game, action: Action) => {
+      const { playerOne, playerTwo } = action.payload;
+      store.playerOne = playerOne;
+      store.playerTwo = playerTwo;
+      localStorage.setItem('playerOne', playerOne);
+      localStorage.setItem('playerTwo', playerTwo);
     },
-    updateScore: (store, action) => {
+    updateScore: (store: Game, action: Action) => {
       const { playerScore } = action.payload;
       if (store.isPlayerOneTurn) {
-        store.totalScore.playerOne = store.totalScore.playerOne + playerScore;
+        store.totalScore.playerOne += playerScore;
       }
       if (!store.isPlayerOneTurn) {
         store.totalScore.playerTwo = store.totalScore.playerTwo + playerScore;
       }
     },
-    changeTurn: (store, action) => {
+    changeTurn: (store: Game, action: Action) => {
       const { turn } = action.payload;
       store.isPlayerOneTurn = turn;
     },

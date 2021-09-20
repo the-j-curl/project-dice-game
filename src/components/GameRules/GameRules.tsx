4e4191232e4c9a2ac7001d8d0ed436c0ba1e40f5
the-j-curl <jamie.p.cook@outlook.com>
2021-09-20 18:21:31 +0200

    Styled game rules

--- src/components/GameRules/GameRules.tsx
+++ src/components/GameRules/GameRules.tsx
@@ -5,7 +5,7 @@ import './GameRules.css';
 export const GameRules: React.FC = () => {
 
   return (
-    <section className="rulesWrapper">
+    <section className="rules-container">
       <h4>Game Rules</h4>
       <p>Each player takes it in turns to roll the dice. The player continues to roll the dice until they choose to hold their current score or they roll a one. When you hold your current turn score your points  will be added to your total score. If you roll a one your turn ends. The first player to score 100 or more wins. Good luck! </p>
     </section>

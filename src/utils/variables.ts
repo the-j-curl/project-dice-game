b0517dcd3e8439255aeed372d2362410ec1baf8a
the-j-curl <jamie.p.cook@outlook.com>
2021-09-20 14:47:12 +0200

    Added variables and new logo images

--- src/utils/variables.ts
+++ src/utils/variables.ts
@@ -1,2 +1,14 @@
 export const headerTitle = 'Pig Dice Game';
-export const logo = 'dice-logo.png';
+
+// Logo
+export const lightLogo = 'light-dice-logo.png';
+export const darkLogo = 'dark-dice-logo.png';
+export const blueLogo = 'blue-dice-logo.png';
+export const darkBlueLogo = 'dark-blue-dice-logo.png';
+export const orangeLogo = 'orange-dice-logo.png';
+
+// Buttons
+export const ROLL_DICE = 'Roll Dice';
+export const HOLD = 'Hold';
+export const RESET_GAME = 'Restart Game';
+export const NEW_GAME = 'New Game';
